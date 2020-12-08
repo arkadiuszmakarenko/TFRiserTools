f1c2b7dc7473bdc3d778973fdfd9689cf60e59ff
arek <agnieszkaarek@gmail.com>
2020-12-08 20:48:05 +0000

    Tidy up.

--- build-vbcc/risermouse.c
+++ build-vbcc/risermouse.c
@@ -2,20 +2,21 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+char *p = (char *)0xBA0006;
+
 int main(int argc, char **argv)
 {
          uint8_t sens = atoi(argv[1]);
-         if (sens>0 &&sens<17)
+         if (sens== 1||sens== 2||sens== 4||sens== 8||sens== 16)
          {
-             char *p = (char *)0xBA0006;
              p[0] = sens;
-
              printf("Sensitivity has been set up to: %u \n", sens);
-
          } 
          else
          {
-             printf("Please provide value in range 1-8, Default is 2. \n");
+             printf("Usage: risermouse <sensitivity value> \n");
+             printf("Current sensitivity value %u \n",p[0]);
+             printf("Please provide value 1,2,4,8,16; 1 - most sensitive, 16 - least sensitive \n");
          }
          
         exit(EXIT_SUCCESS);
